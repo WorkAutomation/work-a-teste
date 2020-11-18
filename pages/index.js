737ea77c1f194a6847b9dbf6c74126449b755bc0
WorkAutomation <contato@workautomation.com.br>
2020-11-18 10:42:28 -0300

    alteracoes

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home(){
     return(
         <div>
             <h1>Home</h1>
-            <button href="views/teste.js">Trocar página</button>
+            <button href="./views/teste.js">Trocar página</button>
         </div>
     );
 }
